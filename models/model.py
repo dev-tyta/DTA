664d7107d2f483d3797a49b567bca9623b798d0c
dev-tyta <hardeykoryar7@gmail.com>
2022-10-24 15:19:54 +0100

    step to step python script for ML model, In progress

--- models/model.py
+++ models/model.py
@@ -8,5 +8,13 @@ from sklearn.preprocessing import RobustScaler, OrdinalEncoder
 import warnings
 warnings.filterwarnings("ignore")
 
+# loading diabetes data into variable data
 data = pd.read_csv("./dataset/diabetes.csv")
 
+# wrangling and feature extraction.
+data.chol_hdl_ratio = round(data.cholesterol / data.hdl_chol, 2)
+data.waist_hip_ratio = round(data.waist / data.hip, 2)
+
+# renaming wrongly named columns
+data.rename(columns={"weight": "height", "height": "weight"}, inplace=True)
+data.height_weight = round(data.height / data.weight)
