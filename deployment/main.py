1c01120e0122220c3bbd3a46b20d8528f0c4af70
dev-tyta <hardeykoryar7@gmail.com>
2022-10-27 04:30:58 +0100

    pickle issues

--- deployment/main.py
+++ deployment/main.py
@@ -1,14 +1,14 @@
 # importing python modules.
 import streamlit as st
-import pickle
-import joblib
+import pickle as pkl
 
 # loading pickle files gotten from model
-lightgbm_pickle = open("lightgbm", "rb")
-lgbm_model = joblib.load(lightgbm_pickle)
+lightgbm_pickle = open("lightgbm.pkl", "rb")
+lgbm_model = pkl.load(lightgbm_pickle)
 
 score = lgbm_model.score()
 
+# create a function to
 st.title("Diabetes Prediction App")
 st.write("Test 1")
 st.write(score)
