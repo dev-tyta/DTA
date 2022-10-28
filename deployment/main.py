74a6dad33961dec741c35906c47136d811829c8e
dev-tyta <hardeykoryar7@gmail.com>
2022-10-28 20:02:58 +0100

    function to predict

--- deployment/main.py
+++ deployment/main.py
@@ -8,8 +8,8 @@ lightgbm_pickle = open("./lightgbm.pickle", "rb")
 lgbm_model = pkl.load(lightgbm_pickle)
 
 
-def predict():
-    pred = []
+def predict(var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15):
+    pred = [var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15]
     np_pred = np.array(pred)
     score = lgbm_model.predict(np_pred)
     return score
