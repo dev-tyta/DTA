695e90c34927b8a7bc5f840e3c485f002db750ce
dev-tyta <hardeykoryar7@gmail.com>
2022-10-28 13:36:59 +0100

    main to create a test model for streamlit to test on

--- deployment/main.py
+++ deployment/main.py
@@ -1,12 +1,16 @@
 # importing python modules.
 import streamlit as st
 import pickle as pkl
+import numpy as np
 
 # loading pickle files gotten from model
 lightgbm_pickle = open("./lightgbm.pickle", "rb")
 lgbm_model = pkl.load(lightgbm_pickle)
 
-score = lgbm_model.score()
+pred = []
+np_pred = np.array(pred)
+score = lgbm_model.predict(np_pred)
+
 
 # create a function to
 st.title("Diabetes Prediction App")
