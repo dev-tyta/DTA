cc7d9691e31831d607171b70101c68d697140701
dev-tyta <hardeykoryar7@gmail.com>
2022-10-31 14:26:32 +0100

    there is an issue with this, work for 14 inputs not 15. WHY???

--- deployment/main.py
+++ deployment/main.py
@@ -7,7 +7,7 @@ import numpy as np
 lightgbm_pickle = open("./lightgbm.pickle", "rb")
 lgbm_model = pkl.load(lightgbm_pickle)
 
-var = [200, 80, 67, 2.99, 20, 1.0, 60, 160, 24.6, 120, 60, 32, 38, 0.84, 2.67]
+var = [200, 80, 67, 2.99, 20, 1.0, 60, 160, 24.6, 120, 60, 32, 38, 0.84]
 
 
 def predict(var_name):
