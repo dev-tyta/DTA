720c2f5fa7b7065aee2fb70dbcccb6ca94040697
dev-tyta <hardeykoryar7@gmail.com>
2022-11-08 21:18:13 +0100

    Update model.py

--- models/model.py
+++ models/model.py
@@ -13,13 +13,13 @@ warnings.filterwarnings("ignore")
 # loading diabetes data into variable data
 data = pd.read_csv("./dataset/diabetes.csv")
 
-# wrangling and feature extraction.
+# wrangling dataset.
 data.chol_hdl_ratio = round(data.cholesterol / data.hdl_chol, 2)
 data.waist_hip_ratio = round(data.waist / data.hip, 2)
 
 # renaming wrongly named columns
 data.rename(columns={"weight": "height", "height": "weight"}, inplace=True)
-data["height_weight"] = round(data.height / data.weight, 2)
+
 
 # correcting comma separated number to decimal separated number.
 data.bmi = pd.to_numeric(data.bmi.str.replace(",", "."))
